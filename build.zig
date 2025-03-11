3357f2cfc82761010ff2daed1d9c2289541af884
Tim <57221600+TiltedToast@users.noreply.github.com>
2025-03-11 23:56:32 +0100

    incremental build

--- build.zig
+++ build.zig
@@ -112,7 +112,12 @@ pub fn build(b: *std.Build) !void {
 
     exe.linkSystemLibrary("cuda");
 
-    b.installArtifact(exe);
+    const no_bin = b.option(bool, "no-bin", "skip emitting binary") orelse false;
+    if (no_bin) {
+        b.getInstallStep().dependOn(&exe.step);
+    } else {
+        b.installArtifact(exe);
+    }
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
